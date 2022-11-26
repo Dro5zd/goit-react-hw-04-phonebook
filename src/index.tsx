8442bc056dc38f9a857e02d19bf9e18da62463a9
drozzzzzd <adrobotyuk@gmail.com>
2022-11-26 12:00:11 +0600

    :tada: Before refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {FeedbackApp} from './FeedbackApp';
-import {Global} from './GlobalStyles';
+import {App} from './App';
+import './index.css';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <FeedbackApp />
-      <Global />
+      <App />
   </React.StrictMode>
 );
